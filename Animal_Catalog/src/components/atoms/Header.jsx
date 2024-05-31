87eb2c64c46f3f400f7ba4541a2354132a969a40
IlviCumani <ilviCumani@gmail.com>
2024-05-31 17:31:33 +0200

    The header links are replaced by an icon when the screen size becometh too small.

--- Animal_Catalog/src/components/atoms/Header.jsx
+++ Animal_Catalog/src/components/atoms/Header.jsx
@@ -3,8 +3,23 @@ import "../../styles/atoms/Header.css";
 
 import { Link } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
+import { IoMenu } from "react-icons/io5";
+
+import { useState } from "react";
 
 export default function Header({ title, navLinks = [] }) {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const handleMenuClick = () => {
+		setIsMenuOpen(prev => !prev);
+	}
+
+	const closeMenu = () => {
+		setIsMenuOpen(false);
+	}
+
+	console.log(isMenuOpen);
+
 	return (
 		<header className="header">
 			<div className="header-logo">
@@ -14,20 +29,28 @@ export default function Header({ title, navLinks = [] }) {
 				<h1 className="header__title">{title}</h1>
 			</div>
 
-			{navLinks && <nav className="header-nav">
-				{navLinks.map((link, index) => (
-					<NavHashLink
-						key={index}
-						to={link.to}
-						className="header-navlinks-link"
-						smooth
-						duration={500}
-					>
-						{link.text}
-					</NavHashLink>
-				))}
-			</nav>}
+			{navLinks.length > 0 && (
+				<span className="header-menu-icon-container">
+					<IoMenu className="header-menu-icon" onClick={handleMenuClick}/>
+				</span>
+			)}
 
+			{navLinks && (
+				<nav className={`header-nav ${isMenuOpen ? "open-menu" : ""}`}>
+					{navLinks.map((link, index) => (
+						<NavHashLink
+							key={index}
+							to={link.to}
+							className="header-navlinks-link"
+							smooth
+							duration={500}
+							onClick={closeMenu}
+						>
+							{link.text}
+						</NavHashLink>
+					))}
+				</nav>
+			)}
 		</header>
 	);
 }
